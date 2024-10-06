72dae5f90ecfd8f01b263c7ed6e0a2bfc2eabb64
RodrDuffoo <20210896@aloe.ulima.edu.pe>
2024-10-06 18:58:57 -0500

    añandiendo nuevas views

--- elimapass/urls.py
+++ elimapass/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('tarjetas/', TarjetaList.as_view(), name='tarjeta-list'),
     path('tarjetas/<str:pk>/', TarjetaDetail.as_view(), name='tarjeta-detail'),
+    path('tarjetas/<str:codigo_tarjeta>/saldo/', SaldoTarjetaView.as_view(), name='saldo-tarjeta'),
+    path('tarjetas/<str:codigo_tarjeta>/viajes/', ListaViajesPorTarjetaView.as_view(), name='lista-viajes-tarjeta'),
+    path('tarjetas/<str:codigo_tarjeta>/cambiar-limite/', CambiarLimiteTarjetaView.as_view(), name='cambiar-limite-tarjeta'),
 
     path('recargas/', RecargaList.as_view(), name='recarga-list'),
     path('recargas/<uuid:pk>/', RecargaDetail.as_view(), name='recarga-detail'),
--- elimapass/views.py
+++ elimapass/views.py
@@ -5,9 +5,20 @@ from django.contrib.auth.hashers import make_password, check_password
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Usuario
+from .models import *
 from .serializer import SignUpSerializer, LoginSerializer
 
+class SaldoTarjetaView(APIView):
+    def get(self, request, codigo_tarjeta):
+        try:
+            tarjeta = Tarjeta.objects.get(codigo=codigo_tarjeta)
+            return Response({
+                "codigo_tarjeta": tarjeta.codigo,
+                "saldo": tarjeta.saldo
+            }, status=status.HTTP_200_OK)
+        except Tarjeta.DoesNotExist:
+            return Response({"error": "Tarjeta no existe"}, status=status.HTTP_404_NOT_FOUND)
+        
 class SignUpView(APIView):
     def post(self, request):
         serializer = SignUpSerializer(data=request.data)
@@ -25,12 +36,60 @@ class LoginView(APIView):
             try:
                 user = Usuario.objects.get(dni=dni)
                 if check_password(password, user.password):
-                    return Response({"id": user.id, "dni": user.dni, "nombres": user.nombres, "apellidos": user.apellidos, "email": user.email}, status=status.HTTP_200_OK)
+                    return Response({
+                        "id": user.id,
+                        "dni": user.dni,
+                        "nombres": user.nombres,
+                        "apellidos": user.apellidos,
+                        "email": user.email,
+                        "tarjeta_id": user.tarjeta.id  
+                    }, status=status.HTTP_200_OK)
                 return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
             except Usuario.DoesNotExist:
                 return Response({"error": "Usuario no existe"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CambiarLimiteTarjetaView(APIView):
+    def put(self, request, codigo_tarjeta):
+        try:
+            tarjeta = Tarjeta.objects.get(codigo=codigo_tarjeta)
+            nuevo_limite = request.data.get('limite')
 
+            if nuevo_limite is not None:
+                tarjeta.limite = nuevo_limite
+                tarjeta.save()
+                return Response({
+                    "codigo_tarjeta": tarjeta.codigo,
+                    "nuevo_limite": tarjeta.limite
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Límite no proporcionado"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Tarjeta.DoesNotExist:
+            return Response({"error": "Tarjeta no existe"}, status=status.HTTP_404_NOT_FOUND)
+        
+class ListaViajesPorTarjetaView(APIView):
+    def get(self, request, codigo_tarjeta):
+        try:
+            tarjeta = Tarjeta.objects.get(codigo=codigo_tarjeta)
+            viajes = Viaje.objects.filter(codigo_tarjeta=tarjeta)
+            
+            lista_viajes = [
+                {
+                    "id": viaje.id,
+                    "fecha_hora": viaje.fecha_hora,
+                    "ruta": viaje.id_tarifa.id_ruta.nombre,
+                    "precio_final": viaje.precio_final
+                }
+                for viaje in viajes
+            ]
+            
+            return Response({
+                "codigo_tarjeta": tarjeta.codigo,
+                "viajes": lista_viajes
+            }, status=status.HTTP_200_OK)
+        except Tarjeta.DoesNotExist:
+            return Response({"error": "Tarjeta no existe"}, status=status.HTTP_404_NOT_FOUND)
 
 class TarjetaList(generics.ListCreateAPIView):
     queryset = Tarjeta.objects.all()
