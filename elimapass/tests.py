f6d2a4f35d8fc779aab68e9935d6df4c0be1d72a
RodrDuffoo <20210896@aloe.ulima.edu.pe>
2024-11-27 03:16:55 -0500

    test

--- elimapass/tests.py
+++ elimapass/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from models import *
+import pytest
+from rest_framework.test import APIClient
+from django.urls import reverse
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_tarjeta_valida_y_limite_proporcionado():
+    client = APIClient()
+    tarjeta = Tarjeta.objects.create(codigo="12345", limite=1000)
+
+    response = client.put(
+        reverse('cambiar-limite', args=[tarjeta.codigo]),
+        {"limite": 5000},
+        format='json'
+    )
+
+    assert response.status_code == 200
+    assert response.data == {"codigo_tarjeta": tarjeta.codigo, "nuevo_limite": 5000}
+
+
